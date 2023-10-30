1a94649c675ab6ae7b3fbb9ca5a6472f6da54955
kysamesa <kysaemesa@gmail.com>
2023-10-30 14:52:53 -0400

    Finished Version

--- lab6.py
+++ lab6.py
@@ -15,16 +15,9 @@ def print_menu():
 
 
 # menu function
-def menu(selection, password):
-    # Selection 1
-    if selection == '1':
-        # Print password encoded
-        print('Your password has been encoded and stored!\n')
-
-    # Selection 2
-    if selection == '2':
-        # Print encoded password and original password
-        print(f'The encoded password is {encode(password)}, and the original password is .\n')
+def menu(encoded_password, decoded_password):
+    # Print encoded password and original password
+    print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.\n')
 
 
 # encode function
@@ -39,10 +32,22 @@ def encode(password):
     # Return the encoded password
     return encoded_password
 
+def decode(password):
+    decoded_password= ''
+    # For loop for decoding password
+    for i in password:
+        # Subtract 3 to each number in password
+        decoded_password+= str(int(i)-3)
+    # Return the encoded password
+    return decoded_password
+
 
 # main method call
 if __name__ == "__main__":
     Continue = True
+    encoded_password = ''
+    decoded_password = ''
+
     # While loop to continue program
     while Continue:
         # Print the menu
@@ -55,7 +60,15 @@ if __name__ == "__main__":
             # If the user entered 1
             if selection == '1':
                 # Prompt the user to enter a password
-                encode(input('Please enter your password to encode: '))
+                encoded_password = encode(input('Please enter your password to encode: '))
+                # Print password encoded
+                print('Your password has been encoded and stored!\n')
+
+            elif selection == '2':
+                # If the user entered 2 as input
+                decoded_password = decode(encoded_password)
+                menu(encoded_password, decoded_password)
+
         else:
             # Set the loop boolean to False to exit the loop
             Continue = False
